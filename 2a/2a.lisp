5e23efd3a41310e2b8845646bb8a6503a1a678f9
Qiao Deng <dengqiao@245.37-191-149.fiber.lynet.no>
2016-09-27 13:33:17 +0200

    handin

--- 2a/2a.lisp
+++ 2a/2a.lisp
@@ -5,7 +5,8 @@
 (defstruct VS 
    matrix 
    (similarity-fn 'dot-product))
-	
+   
+; 2b	
 (defun strip_ipunc (string)  ;strip initial punctuations
     (remove-if-not #'alphanumericp string :start 0 
     :end (position-if #'alphanumericp string)))
@@ -23,21 +24,18 @@
     until (not space)))
 	
 (defparameter *word* (with-open-file (stream "words.txt" :direction :input)
-  (loop
-      for line = (read-line stream nil)
+  (loop for line = (read-line stream nil)
       while line
       append (normalize-token line))))
 	  
 (defparameter *co_word* (with-open-file (stream "brown2.txt" :direction :input)
-  (loop
-      for line = (read-line stream nil)
+  (loop for line = (read-line stream nil)
       while line
 	  if (intersection *word* (normalize-token line) :test #'equal)
       	append (normalize-token line))))  
 						
 (defparameter *myhash* (make-hash-table :test 'equal))	
-(loop
-	for x in *co_word*
+(loop for x in *co_word*
 	if (gethash x *myhash*)
 	do (incf (gethash x *myhash*))
 	else do (setf (gethash x *myhash*) 1))
@@ -68,81 +66,67 @@
 (maphash #'getkeys *myhash*)
 
 (setf m (length *word*))
-(setf n (length *feature*))
-(print "Matrix dimensions:")
-(print m)
-(print n)						   
+(setf n (length *feature*))						   
 
 (defparameter space (make-VS
     :matrix (make-array (list m n) :initial-element 0)))
 	
 (defun update (amatrix slist conlist)
-	(loop
-		for wd in conlist
-		do (update2 amatrix wd slist)))
-(defun update2 (amatrix wd slist)
-	(loop
-		for co_wd in slist
-		if (not (member co_wd *stop-list* :test #'equal))
-		do (incf (aref amatrix (position wd *word* :test #'equal) (position co_wd *feature* :test #'equal)))))
+	(loop for wd in conlist
+		do (loop for co_wd in slist
+			if (not (member co_wd *stop-list* :test #'equal))
+			do (incf (aref amatrix (position wd *word* :test #'equal) (position co_wd *feature* :test #'equal))))))		
 
 (defun read-corpus-to-vs (vspace corpusfile) 
 	(with-open-file (stream corpusfile :direction :input)
-	  (loop
-	      for line = (read-line stream nil)
+	  (loop for line = (read-line stream nil)
 		  while line
 		  if (intersection *word* (normalize-token line) :test #'equal)
 		  do (update (VS-matrix vspace) (normalize-token line) (intersection *word* (normalize-token line) :test #'equal)))))
-	  
+		  
+(defparameter space (make-VS
+	:matrix (make-array (list m n) :initial-element 0)))
+	
 (read-corpus-to-vs space "brown2.txt")	  
-(print "Vector space is built.")
 
 ; 2c
 (defun array-row (arr row)
-    (loop 
-        for i from 0 to (- (first (last (array-dimensions arr))) 1)
+    (loop for i from 0 to (- (first (last (array-dimensions arr))) 1)
         collect (aref arr row i)))
 		
 (defun get-feature-vector (space str)
 	(array-row (VS-matrix space) (position str *word* :test #'equal)))
+	
 ; 2d
 (defun print-features (space str n)
 	(let ((alist nil)
 		(vc (get-feature-vector space str)))
-		(loop 
-			for i from 0 to (- (length *feature*) 1)
+		(loop for i from 0 to (- (length *feature*) 1)
 			do (push (cons (nth i *feature*) (nth i vc)) alist))
 		(subseq (sort alist #'> :key #'cdr) 0 n)))
 		
-;(print (get-feature-vector space "food"))
-;(print (print-features space "university" 9))
+;(get-feature-vector space "food")
+;(print-features space "university" 9)
 
 ; 3a
 (defun euclidean-length (vec)
 	(let ((sum 0))
-		(loop
-			for i from 0 to (- (length vec) 1)
+		(loop for i from 0 to (- (length vec) 1)
 			do (setf sum (+ sum (expt (nth i vec) 2))))
 			(setf sum (sqrt sum))))
 			
-(print (euclidean-length (get-feature-vector space "boston")))
+;(euclidean-length (get-feature-vector space "boston"))
 
 ; 3b
 (defun length-normalize-vs (vspace)
-	(loop
-		for i from 0 to (- m 1)
-		do (length-normalize-vs2 vspace i)))
-		
-(defun length-normalize-vs2 (vspace2 x)
 	(let ((len 0))
-		(setf len (euclidean-length (get-feature-vector vspace2 (nth x *word*))))
-		;(print (cons x len))
-		(loop
-			for j from 0 to (- n 1)
-			do (setf (aref (VS-matrix vspace2) x j) (/ (aref (VS-matrix vspace2) x j) len)))))
+		(loop for i from 0 to (- m 1)
+			do (setf len (euclidean-length (get-feature-vector vspace (nth i *word*))))
+			do (loop for j from 0 to (- n 1)
+					do (setf (aref (VS-matrix vspace) i j) (/ (aref (VS-matrix vspace) i j) len))))))
 		
 (length-normalize-vs space)
-(print (euclidean-length (get-feature-vector space "boston")))
+;(euclidean-length (get-feature-vector space "boston"))
 
 ; 3c
 
@@ -150,10 +134,7 @@
 	(let ((sum 0))	
 		(setf xvec (get-feature-vector space x))
 		(setf yvec (get-feature-vector space y))
-		;(setf conv (intersection xvec yvec :test #'equal))
-		(loop 
-			for i from 0 to (- (length xvec) 1)
-			;if (> (* (nth i xvec) (nth i yvec)) 0)
+		(loop for i from 0 to (- (length xvec) 1)
 			do (setf sum (+ sum (* (nth i xvec) (nth i yvec)))))
 		sum))
 ; 3d		
@@ -161,9 +142,8 @@
 (defun word-similarity (vspace str1 str2)
 	(Funcall (VS-similarity-fn vspace) vspace str1 str2))
 
-
 (print (word-similarity space "university" "college"))
-(print (word-similarity space "china" "college"))	
+
 	
 
 		   
